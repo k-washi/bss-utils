0ba423847bbf7b514d9e96fb1d805dcce3b6a6c0
k-washi <kai.washizaki@gmail.com>
2020-02-01 19:04:35 +0900

    Addnencryption generator

--- encryption/password.go
+++ encryption/password.go
@@ -1,1 +1,43 @@
 package encryption
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"os"
+)
+
+var (
+	salt1 string
+	salt2 string
+)
+
+func init() {
+	salt1 = os.Getenv("GO_CRYPTO_SALT1")
+	salt2 = os.Getenv("GO_CRYPTO_SALT2")
+	if salt1 == "" {
+		fmt.Println("salt1 env valiable is not set")
+		salt1 = "@#$%"
+	}
+	if salt2 == "" {
+		fmt.Println("salt2 env valiable is not set")
+		salt2 = "^&*()"
+	}
+}
+
+//GetMD5 create md5 encryption
+func GetMD5(arg1 string, arg2 string) string {
+	h := md5.New()
+	io.WriteString(h, arg1)
+	pwmd5arg1 := fmt.Sprintf("%x", h.Sum(nil))
+
+	io.WriteString(h, arg2)
+	pwmd5arg2 := fmt.Sprintf("%x", h.Sum(nil))
+
+	io.WriteString(h, salt1)
+	io.WriteString(h, pwmd5arg1)
+	io.WriteString(h, salt2)
+	io.WriteString(h, pwmd5arg2)
+
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
---
+++ encryption/password_test.go
@@ -0,0 +1,42 @@
+package encryption
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/k-washi/bss-utils/uniquegenerator"
+
+	"github.com/k-washi/bss-utils/utils"
+	"github.com/tj/assert"
+)
+
+func TestGetMD5same(t *testing.T) {
+	id := "donaDon^?gaXyz"
+	password := "sksS0t3f36"
+	testNum := 10000
+	base := make([]string, testNum)
+
+	for i := 0; i < testNum; i++ {
+		base[i] = GetMD5(id, password)
+	}
+	uniqueDlid := utils.RemoveUlidDuplicate(base)
+
+	assert.EqualValues(t, len(uniqueDlid), 1)
+
+}
+
+func TestGetMD5diff(t *testing.T) {
+	id := "donaDon^?gaXyz"
+
+	testNum := 10000
+	base := make([]string, testNum)
+
+	for i := 0; i < testNum; i++ {
+		base[i] = GetMD5(id, uniquegenerator.Get())
+	}
+	uniqueDlid := utils.RemoveUlidDuplicate(base)
+
+	assert.EqualValues(t, len(uniqueDlid), testNum)
+
+	fmt.Println("Example:", base[0:5])
+}
